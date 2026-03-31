90366d99f55895ca3c27e734b70b1b04fdb4d118
Musham Suresh <sureshmusham51@gmail.com>
2026-03-31 16:10:19 +0530

    Update S3 bucket for Terraform backend configuration

--- backend/backend.tf
+++ backend/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "your-terraform-state-bucket"
+    bucket         = "suresh-terraform-state-2026"
     key            = "ec2-project/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "terraform-lock"
