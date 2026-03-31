e0bc5eea315e1e068d33c74ae8579df6c4ad8d83
Musham Suresh <sureshmusham51@gmail.com>
2026-03-31 16:41:09 +0530

    Delete backend directory

--- backend/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "suresh-terraform-state-2026"
-    key            = "ec2-project/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "terraform-lock"
-  }
-}
